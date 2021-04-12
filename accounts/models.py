d92aef6bf121ef932442607ebf3ccc5df919fe04
Leonard Colin <leooo.colin@gmail.com>
2021-04-13 01:43:18 +0200

    Create User authentication model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserAuth(AbstractUser):
+    """
+    Class that inherit from django AbstractUser to store users in database
+    """
+
+    email = models.EmailField(
+        unique=True,
+        error_messages={
+            'unique': "Un utilisateur avec cet email existe déjà.",
+        },
+    )
