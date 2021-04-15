8a55a4618fffb0f5876e3d18958c8dd02fcc30ec
Leonard Colin <leooo.colin@gmail.com>
2021-04-16 00:38:46 +0200

    Add accounts unit tests models

---
+++ accounts/tests/unit/test_models.py
@@ -0,0 +1,15 @@
+from mixer.backend.django import mixer
+import pytest
+from accounts.models import UserAuth
+
+
+@pytest.mark.django_db
+class TestModels:
+    """Class that tests account models"""
+
+    def test_user_auth(self):
+        """Test a new user is in database"""
+
+        user = mixer.blend(UserAuth, username='test user')
+        assert user.username == 'test user'
+        assert UserAuth.objects.filter(username='test user').exists() is True
