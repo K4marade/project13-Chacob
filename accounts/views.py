f25954729bc55094fb21fe791d0b059ad9846e7d
Leonard Colin <leooo.colin@gmail.com>
2021-04-15 19:28:08 +0200

    Fake failed test

--- accounts/views.py
+++ accounts/views.py
@@ -32,7 +32,7 @@ def register_view(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
+            username = form.cleaned_data.get('sername')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
