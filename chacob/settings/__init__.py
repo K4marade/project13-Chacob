e8f555917ec7eaacdafa29229a1164cfe9d8cdca
Leonard Colin <leooo.colin@gmail.com>
2021-04-20 14:40:55 +0200

    Config django smtp email

--- chacob/settings/__init__.py
+++ chacob/settings/__init__.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import environ
 
-# Initialise environment variables
 from django.urls import reverse_lazy
 from django.contrib import messages
 
+# Initialise environment variables
 env = environ.Env()
 environ.Env.read_env()
 
@@ -141,3 +141,10 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('GMAIL_USER')
+EMAIL_HOST_PASSWORD = env('GMAIL_PASS')
