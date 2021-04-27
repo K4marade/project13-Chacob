03e5f9c5f58b8e62da583999ecf6752ac5ee9e38
Leonard Colin <leooo.colin@gmail.com>
2021-04-27 13:35:32 +0200

    Update mycalendar css style

--- chacob/settings/__init__.py
+++ chacob/settings/__init__.py
@@ -143,9 +143,13 @@ MESSAGE_TAGS = {
     messages.ERROR: 'alert-danger',
 }
 
+# Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env('GMAIL_USER')
 EMAIL_HOST_PASSWORD = env('GMAIL_PASS')
+
+# Crispy forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
