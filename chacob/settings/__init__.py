7ad6cca574826bbb1145d931d436334b136dc3d1
Leonard Colin <leooo.colin@gmail.com>
2021-05-01 19:25:01 +0200

    Update production settings

--- chacob/settings/__init__.py
+++ chacob/settings/__init__.py
@@ -133,9 +133,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
 
 # Login redirect
 LOGIN_REDIRECT_URL = reverse_lazy('home')
--- chacob/settings/production.py
+++ chacob/settings/production.py
@@ -1,15 +1,18 @@
 from . import *
 
-SECRET_KEY = env('SECRET_KEY')
+load_dotenv()
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = ['134.209.92.2']
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWD'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
