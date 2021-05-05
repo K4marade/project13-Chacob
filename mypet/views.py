84100f7a5c9173fe35393ec1a8174981c9a38c7b
Leonard Colin <leooo.colin@gmail.com>
2021-05-05 14:59:53 +0200

    Fix mypet and mycalendar views

--- mypet/views.py
+++ mypet/views.py
@@ -10,7 +10,6 @@ def create_pet_view(request):
 
     if request.method == "GET":
         form = AddPetForm()
-        return render(request, "mypet.html", locals())
     elif request.method == "POST":
         form = AddPetForm(request.POST, request.FILES)
         if form.is_valid():
@@ -20,6 +19,8 @@ def create_pet_view(request):
             messages.success(request, pet.name + " a bien été ajouté !")
             form = AddPetForm()
             return redirect('mypet')
+    return render(request, "mypet.html", locals())
+
         # else:
         #     error = form.errors
         #     form = AddPetForm(request.POST or None, request.FILES or None)
@@ -30,7 +31,6 @@ def update_pet_view(request, id_pet):
     instance_pet = Pet.objects.get(pk=id_pet)
     if request.method == "GET":
         form = EditPetForm(instance=instance_pet)
-        return render(request, 'update_my_pet.html', locals())
     elif request.method == "POST":
         form = EditPetForm(request.POST, request.FILES, instance=instance_pet)
 
@@ -42,6 +42,7 @@ def update_pet_view(request, id_pet):
         # else:
         #     error = form.errors
         #     return render(request, "mypet.html", locals())
+    return render(request, 'update_my_pet.html', locals())
 
 
 def delete_pet_view(request, id_pet):
