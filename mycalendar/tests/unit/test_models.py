d27e40e8c1e930a8aab18ff3d29fc798c4d95ee2
Leonard Colin <leooo.colin@gmail.com>
2021-05-07 17:36:02 +0200

    Add unit and integration tests for mycalendar

--- mycalendar/tests/unit/test_models.py
+++ mycalendar/tests/unit/test_models.py
@@ -1,6 +1,9 @@
 from mixer.backend.django import mixer
 import pytest
+
+from accounts.models import UserAuth
 from mycalendar.models import Event
+from mypet.models import Pet
 
 
 @pytest.mark.django_db
@@ -14,3 +17,27 @@ class TestModels:
         assert event_reason.reason == 'Vaccin'
         assert Event.objects.filter(reason='Vaccin').exists() is True
         assert Event.objects.count() == 1
+
+    def test_str_event(self):
+        """Test the string method from event model"""
+        user = UserAuth.objects.create(username="Leonard",
+                                       password="Testing1234",
+                                       email="test@test.com")
+
+        user_id = UserAuth.objects.get(id=user.id)
+
+        pet = Pet.objects.create(user_id=user_id.id,
+                                 species="cat", gender="male",
+                                 birth_date="2021-05-05",
+                                 name="Felix")
+
+        event = Event.objects.create(user_id_id=user_id.id,
+                                     date="2021-05-05 14:42:00+02",
+                                     pet_name_id=pet.id,
+                                     reason="Vaccin",
+                                     mail_alert=False)
+
+        assert str(event) == "{}, {}, {}, {}".format(event.user_id.username,
+                                                     event.date,
+                                                     event.pet_name.name,
+                                                     event.mail_alert)
