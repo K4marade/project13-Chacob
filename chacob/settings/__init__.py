b48e776c3f4f856879fd5670eecd10c50ea4d470
leonardtkt <leonard@tkt.paris>
2022-09-22 18:05:30 +0200

    feat: Install django-browser-reload

--- chacob/settings/__init__.py
+++ chacob/settings/__init__.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_browser_reload',
     'accounts',
     'mycalendar',
     'home',
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'chacob.urls'
--- chacob/urls.py
+++ chacob/urls.py
@@ -27,3 +27,8 @@ urlpatterns = [
     path('mypet/', include('mypet.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns = [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ] + urlpatterns
